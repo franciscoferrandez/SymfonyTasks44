9a89037de07708fe74e0ad435daebc045c78ad80
Francisco Ferrández <francisco.ferrandez@nunsys.com>
2020-08-27 20:08:55 +0200

    Customer migrations

---
+++ migrations/Version20200825194049.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200825194049 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE customer (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(255) DEFAULT \'\' NOT NULL, commercial_name VARCHAR(255) DEFAULT \'\' NOT NULL, acronym VARCHAR(6) DEFAULT NULL, back_color VARCHAR(25) DEFAULT \'#444444\' NOT NULL, fore_color VARCHAR(25) DEFAULT \'#eeeeee\' NOT NULL, INDEX IDX_81398E09A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE customer ADD CONSTRAINT FK_81398E09A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE customer');
+    }
+}
